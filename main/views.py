6806130d37cfe349b5516f80f0839c9bd544c5d0
Marcin Ciesielski <marcin.ciesielski@pw.edu.pl>
2018-12-28 15:28:26 +0100

    Change View and Validate date of booking

--- main/views.py
+++ main/views.py
@@ -2,11 +2,26 @@ from django.db.models import Q
 from main.models import Room, Booking
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
-
+from django.views import View
+from django.shortcuts import render, get_object_or_404
+import datetime
 
 class RoomListView(ListView):
     model = Room
     ordering = ['-capacity']
+
+
+class MyRoomListView(View):
+    template_name = 'main/room_list.html'
+    queryset = Room.objects.all()
+    date = datetime.datetime.now().date()
+
+    def get(self, request, *args, **kwargs):
+        context = {
+            'room_list': self.queryset,
+            'date': self.date, # date.strftime('%Y-%m-%d'),
+        }
+        return render(request, self.template_name, context)
 
 
 class RoomDetailView(DetailView):
@@ -50,10 +65,10 @@ class SearchRoomView(ListView):
         query = method_dict.get('q', None) # method_dict['q']
         print(query)
         if query is not None:
-            lookups = Q(name__icontains=query) | Q(capacity__icontains=query)
+            lookups = Q(name__icontains = query) | Q(capacity__icontains = query)
             return Room.objects.filter(lookups).distinct()
         return Room.objects.all().order_by('-capacity')
-        '''
-        __icontains = field contains this
-        __iexact = fields is exactly this
-        '''
+
+        # __icontains = field contains this
+        #__iexact = fields is exactly this
+
